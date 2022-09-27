bb1a9ed3f97cea03d1a889099f2942d5437bcc50
Luis <l-serrato@live.com>
2022-09-27 16:39:08 -0300

    desafio9

--- src/challenges.js
+++ src/challenges.js
@@ -75,11 +75,13 @@ function fizzBuzz(nums) {
 } 
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let code = string.replace(/a/g, 1).replace(/e/g, 2).replace(/i/g, 3).replace(/o/g, 4).replace(/u/g, 5);
+  return code;
 }
-function decode() {
-  // seu código aqui
+function decode(sentence) {
+  let cypher = sentence.replace(/1/g, 'a').replace(/2/g, 'e').replace(/3/g, 'i').replace(/4/g, 'o').replace(/5/g, 'u');
+  return cypher;
 }
 
 // Desafio 10
