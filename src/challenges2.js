1416c81cf2197439def000b82dfe1724fd50baa7
Luis <l-serrato@live.com>
2022-09-27 17:25:05 -0300

    desafio13

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,23 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(sequence) {
+  if (sequence.length !== 11){
+    return 'Array com tamanho incorreto.'
+  }
+  for (let num of sequence){
+    if (num < 0 || num > 9){
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let sequenceSorted = sequence.slice(0);
+  sequenceSorted.sort(function (a, b) { return a - b});
+  for (let i = 0; i <=8; i += 1){
+    if(sequenceSorted[i] === sequenceSorted[i + 2]){
+      message = 'não é possível gerar um número de telefone com esses valores';
+    }
+    }
+  return message;
+
+  let number = ' ';
 }
 
 // Desafio 12
@@ -12,8 +29,16 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(drinks) {
+  let numberOfDrinks = drinks.match(/\d+/g).map(Number);
+  let glassesOfWater = numberOfDrinks.reduce((a, b) => a + b, 0);
+  let total = '';
+  if (glassesOfWater === 1){
+    total = `${glassesOfWater} copo de água`;
+  }else {
+    total = `${glassesOfWater} copos de água`;
+  }
+  return total;
 }
 
 module.exports = {
